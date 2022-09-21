d1cf79423dbc08dbe60ca943d4e85f8df1fe0b0b
Maelys Champeau <maelys.champeau@yahoo.fr>
2022-09-21 12:25:59 +0200

    deploy app

---
+++ src/components/Hi.jsx
@@ -0,0 +1,14 @@
+import React from "react"
+import Card from "./Card.jsx"
+
+const Hi = () => {
+  return (
+    <div className="accueil">
+      <p className="presentation_text">Hello,<br />I am Maëlys<br />
+        <span>a web developer</span>
+      </p>
+      <Card/>
+    </div>
+  )
+}
+export default Hi
---
+++ src/pages/Contact.jsx
@@ -0,0 +1,10 @@
+
+const Contact = () => {
+  return (
+    <div>
+      <p> test</p>
+    </div>
+  )
+}
+
+export default Contact
